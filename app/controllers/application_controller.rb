99459097edcf20a8af46f2e74418ce8630a8914e
marinacor1 <marinaspam1234@gmail.com>
2016-05-11 11:23:34 -0600

    fully functional allow method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,24 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  add_flash_types :success, :info, :warning, :danger #this method will allow us 
+  add_flash_types :success, :info, :warning, :danger
 
   helper_method :current_user
 
+  before_action :authorize!
+
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def current_permission
+    @current_permission ||=  PermissionsService.new(current_user, params[:controller], params[:action])
+  end
+
+  def authorize!
+    unless current_permission.allow?
+      redirect_to root_url, danger: "You shall not pass!"
+    end
   end
 end
--- app/services/permissions_service.rb
+++ app/services/permissions_service.rb
@@ -1,22 +1,44 @@
 class PermissionsService
-  def initialize(user, controller, action)
-    @_user = user
-    @_controler = controller
-    @_action = action #underscores says that variable will be accessed through a method
+  def initialize(user, controller, action) #pass user object or id? debatable
+    @_user = user || User.new
+    @_controller = controller
+    @_action = action
   end
 
+  def allow?
+    if user.platform_admin?
+      return true if controller = "sessions"
+      return true if controller == "items" && action.in?(%w( index show))
+      return true if controller == "orders" && action.in?(%w( index show))
+      return true if controller == "users" && action.in?(%w( index show))
+      return true if controller == "stores" && action.in?(%w( index show))
+    elsif user.store_admin?
+      return true if controller == "items" && action.in?(%w( index show))
+      return true if controller == "orders" && action.in?(%w( index show))
+      return true if controller == "stores" && action.in?(%w( index show))
+    elsif user.registered_user?
+      return true if controller == "items" && action.in?(%w( index show))
+      return true if controller == "stores" && action.in?(%w( index show))
+      return true if controller == "orders" && action.in?(%w( show))
+    else
+      return true if controller == "stores" && action == "index"
+      return true if controller == "sessions" && action == "new"
+      return true if controller == "sessions" && action == "create"
+      return true if controller == "sessions" && action == "destroy"
+    end
+  end
 
   private
 
-    def controller
-      @_controller
-    end
+  def controller
+    @_controller
+  end
 
-    def action
-      @_action
-    end
+  def action
+    @_action
+  end
 
-    def user
-      @_user
-    end
+  def user
+    @_user
+  end
 end
